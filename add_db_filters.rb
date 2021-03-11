de74673be233043f0333519de6507efa638f2ea3
Nicholas Galauxy <nicholas@galauxy.com>
2021-03-11 15:12:58 -0600

    Update add_db_filters.rb

--- add_db_filters.rb
+++ add_db_filters.rb
@@ -70,12 +70,8 @@ begin
   }
   @json['other_seeds']['features'].push(*new_features)
   new_config = JSON.pretty_generate(@json)
-  civis_file = post_url("#{ENV['CIVIS_API_ENDPOINT']}/files", {name: 'config.json'})
-  puts civis_file
-  puts civis_file.class
-  puts civis_file.keys
-  puts civis_file['uploadFields']
-  upload_fields = JSON.parse(civis_file['uploadFields'])
+  civis_file = JSON.parse(post_url("#{ENV['CIVIS_API_ENDPOINT']}/files", {name: 'config.json'}))
+  upload_fields = civis_file['uploadFields']
   upload_fields['file'] = new_config
   post_url(civis_file['uploadUrl'], upload_fields)
   # civis_file_id = post_url("#{ENV['CIVIS_API_ENDPOINT']}/json_values", {name: 'config.json', valueStr: new_config})
